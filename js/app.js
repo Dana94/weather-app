b8067bd112f310016382321c8d4d6cf4d2b96c31
Dana Ottaviani <dana.ottaviani@gmail.com>
2017-02-12 17:43:24 -0500

    downloaded simpleweather.js and working on changing symbols with weather conditions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,43 @@
+let icons = ['fa-sun','fa-thermometer-full', 'fa-snowflake-o','fa-cloud', 'fa-bolt', 'fa-moon-o'];
+let weather = ['snow','sunny', 'rainy', 'cloudy', 'night', 'sunset-night'];
 
+$('#icons').hide();
 //Source: https://www.freecodecamp.com/challenges/get-geolocation-data
 if (navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
-      $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+      loadWeather(position.coords.latitude+','+position.coords.longitude, '');
     })
   }
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'f',
+    success: function(weather) {
+      let html = '<h1 id="city-region">'+weather.city+', '+weather.region+'</h1>';
+      html += '<h1 id="temps">'+ weather.temp +'&deg;'+ weather.units.temp + ', ' + weather.alt.temp +'&deg;C</h1>';
+      html += '<h1 id="condition">'+weather.currently+'</h1>';
+      
+      
+      $("#location").html(html);
+      $('#icons').html('<i class="fa fa-snowflake-o fa-4x" aria-hidden="true"></i>');
+      $('#icons').show();
+    },
+    error: function(error) {
+      $("#location").html('<p>'+error+'</p>');
+    }
+  });
+}
+
+
+function getSymbols(){
+	let condition = document.getElementById('condition').innerhtml.split(' ');
+	console.log('hi');
+	// if(condition.indexOf('snow') != -1){
+	// 	return '<i class="fa fa-snowflake-o fa-4x" aria-hidden="true"></i>';
+	// }
+}
+
+
+document.getElementById('location').addEventListener(onchange, getSymbols);
